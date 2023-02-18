1aea235c423f48811a116d03ec1707ef9eb05264
Gurjot Sidhu <stuff@thatgurjot.com>
2023-02-18 20:21:11 +0530

    v1 release

--- README.md
+++ README.md
@@ -2,9 +2,15 @@
 Opens the current Git repository's remote URL in a browser.
 
 ## Usage
-1. Run `Open Remote URL in Browser` from the Command Palette.
-2. Select the remote you want to open.
+The extension adds two commands to the Command Palette –
+1. `Open Remote Fetch URL in Browser`
+2. `Open Remote Push URL in Browser`
 
+Selecting either of these commands will present a list of remotes to choose from.
+
+Selecting a remote will present a list of browser options to choose from.
+
+Selecting a browser will open the remote URL in the selected browser.
 
 ## CHANGELOG
-1. 0.0.1 - Initial release
+1. 1.0.0 - Initial release
--- extension.js
+++ extension.js
@@ -1,76 +1,65 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
 const util = require('util');
 const exec = util.promisify(require('child_process').exec);
-
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	// console.log('Congratulations, your extension "open-in-browser" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('open-in-browser.openRemoteURLInBrowser', function () {
-		// The code you place here will be executed every time your command is executed
-
-		// get all remotes for the current repo
-		let remoteNames, fetchURL, pushURL = '';
-		getAllRemotes().then((remotes) => {
-			remoteNames = remotes;
-			console.log(remotes);
+	// Remote Fetch URL
+	let disposableFetch = vscode.commands.registerCommand('open-in-browser.openRemoteFetchURLInBrowser', function () {
+		// get all remotes for the current repository
+		getAllRemotes().then((result) => {
+			let remoteNames = result.trim().split('\n');
+			vscode.window.showQuickPick(remoteNames).then((remote) => {
+				// get fetch URL for the chosen remote
+				getFetchURL(remote).then((url) => {
+					showBrowserOptions(url);
+				});
+			});
 		});
-
-		// // get fetch URL from git remote -v
-		// getFetchURL().then((url) => {
-		// 	fetchURL = url;
-		// });
-		// // getPushURL().then((url) => {
-		// // 	pushURL = url;
-		// // });
-
-		// // // show options to user
-		// vscode.window.showQuickPick(['(Default Browser)', 'Arc', 'Firefox', 'Google Chrome', 'Safari']).then((option) => {
-		// 	if (option === 'Default') {
-		// 		// open fetch URL in default browser
-		// 		vscode.env.openExternal(vscode.Uri.parse(fetchURL));
-		// 	} else {
-		// 		// open fetch URL in the chosen browser
-		// 		exec("open -a '" + option + "' " + fetchURL);
-		// 	}
-		// });
+	});
+	
+	// Remote Push URL
+	let disposablePush = vscode.commands.registerCommand('open-in-browser.openRemotePushURLInBrowser', function () {
+		// get all remotes for the current repository
+		getAllRemotes().then((result) => {
+			let remoteNames = result.trim().split('\n');
+			vscode.window.showQuickPick(remoteNames).then((remote) => {
+				// get fetch URL for the chosen remote
+				getPushURL(remote).then((url) => {
+					showBrowserOptions(url);
+				});
+			});
+		});
 	});
 
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(disposableFetch);
+	context.subscriptions.push(disposablePush);
+}
+
+function showBrowserOptions(url) {
+	// show browser options to user
+	vscode.window.showQuickPick(['(Default Browser)', 'Arc', 'Firefox', 'Google Chrome', 'Safari']).then((option) => {
+		if (option === '(Default Browser)') {
+			// open fetch URL in default browser
+			vscode.env.openExternal(vscode.Uri.parse(url));
+		} else {
+			// open fetch URL in the chosen browser
+			exec("open -a '" + option + "' " + url);
+		}
+	});
 }
 
 async function getAllRemotes() {
 	const currentPath = vscode.workspace.workspaceFolders[0].uri.path;
-	const { stdout, stderr } = await exec("cd " + currentPath + " && git remote | wc -l | awk '{print $1}'");
-	const numRemotes = stdout;
-	let remoteNames = [];
-	for (let i = 0; i < parseInt(numRemotes); i++) {
-		const { stdout, stderr } = await exec("cd " + currentPath + " && git remote | awk '{print $" + i + "}'");
-		remoteNames.push(stdout);
-	}
-	if (stderr) {
-		vscode.window.showInformationMessage(stderr.toString());
-		return stderr;
-	}
-	return remoteNames;
+	const {stdout, stderr} = await exec("cd " + currentPath + " && git remote")
+	return stdout;
 }
 
-async function getFetchURL() {
+async function getFetchURL(remote) {
 	const currentPath = vscode.workspace.workspaceFolders[0].uri.path;
-	const { stdout, stderr } = await exec("cd " + currentPath + " && git remote -v | grep fetch | awk '{print $2}'");
+	const { stdout, stderr } = await exec("cd " + currentPath + " && git config --get remote." + remote + ".url");
   if (stderr) {
 		vscode.window.showInformationMessage(stderr.toString());
 		return stderr;
@@ -78,9 +67,9 @@ async function getFetchURL() {
 	return stdout.trim();
 }
 
-async function getPushURL() {
+async function getPushURL(remote) {
 	const currentPath = vscode.workspace.workspaceFolders[0].uri.path;
-	const { stdout, stderr } = await exec("cd " + currentPath + " && git remote -v | grep push | awk '{print $2}'");
+	const { stdout, stderr } = await exec("cd " + currentPath + " && git remote get-url --push " + remote);
   if (stderr) {
 		vscode.window.showInformationMessage(stderr.toString());
 		return stderr;
